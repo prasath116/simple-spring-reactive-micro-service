112efd3e3f37417468842418dc5ae675629e021a
Prasath M <prasath116@gmail.com>
2021-12-31 13:36:39 +0530

    2
    2

---
+++ employee-service/src/main/java/com/prs/services/employee/controller/EmployeeController.java
@@ -0,0 +1,55 @@
+package com.prs.services.employee.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prs.services.employee.model.Employee;
+import com.prs.services.employee.repository.EmployeeRepository;
+
+@RestController
+public class EmployeeController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
+	
+	@Autowired
+	EmployeeRepository repository;
+	
+	@PostMapping("/")
+	public Employee add(@RequestBody Employee employee) {
+		LOGGER.info("Employee add: {}", employee);
+		return repository.add(employee);
+	}
+	
+	@GetMapping("/{id}")
+	public Employee findById(@PathVariable("id") Long id) {
+		LOGGER.info("Employee find: id={}", id);
+		return repository.findById(id);
+	}
+	
+	@GetMapping("/")
+	public List<Employee> findAll() {
+		LOGGER.info("Employee find");
+		return repository.findAll();
+	}
+	
+	@GetMapping("/department/{departmentId}")
+	public List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId) {
+		LOGGER.info("Employee find: departmentId={}", departmentId);
+		return repository.findByDepartment(departmentId);
+	}
+	
+	@GetMapping("/college/{collegeId}")
+	public List<Employee> findByCollege(@PathVariable("collegeId") Long collegeId) {
+		LOGGER.info("Employee find: collegeId={}", collegeId);
+		return repository.findByCollege(collegeId);
+	}
+	
+}
